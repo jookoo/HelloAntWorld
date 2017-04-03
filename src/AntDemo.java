91a50b75db59604ce5ca108078ab824e29e35fc3
joshuakornfeld <joshuakornfeld@nb-jkornfeld01.bur-kg.org>
2017-04-03 13:13:09 +0200

    Ant Demo mit Buildscript

---
+++ src/AntDemo.java
@@ -0,0 +1,14 @@
+
+/**
+ * Ein Hello World Beispiel.
+ * 
+ * @author joshuakornfeld
+ *
+ */
+public class AntDemo {
+
+	public static void main(final String[] args) {
+		System.out.println("You managed to succeed.");
+	}
+
+}
